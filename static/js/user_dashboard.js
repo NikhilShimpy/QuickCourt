d153ea88a55c1f958a7b76933d7ba8aa4002a214
Nikhil Shimpy <nikhilshimpy712@gmail.com>
2025-08-14 19:33:52 +0530

    Update user_dashboard.js

--- static/js/user_dashboard.js
+++ static/js/user_dashboard.js
@@ -1,13 +1,14 @@
 // Initialize Firebase with your config
 const firebaseConfig = {
-  apiKey: "AIzaSyDH9cXqMCeNmYbN25aI8ygrsz1AbvmrGm8",
-  authDomain: "streetconnect-54453.firebaseapp.com",
-  projectId: "streetconnect-54453",
-  storageBucket: "streetconnect-54453.appspot.com",
-  messagingSenderId: "301987641886",
-  appId: "1:301987641886:web:e9fd56c486bb16ecb78c0f",
-  measurementId: "G-BLENMZN245"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
